e8177c1cb7451f284cfbfeb2d94ef35c24f0d184
Robert Guss <robert.guss@insuremytrip.com>
2020-05-01 09:29:32 -0400

    refactor: wrapped gatsby node https requests in try catch with http exception handler

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -26,10 +26,9 @@ exports.sourceNodes = async (
    */
   if (customUrls) {
     for (const key in customUrls) {
-      const customUrlName = key;
-      const apiUrl = customUrls[key];
-
       try {
+        const customUrlName = key;
+        const apiUrl = customUrls[key];
         const response = await fetch(apiUrl);
         const { data } = await response.json();
 
@@ -61,26 +60,30 @@ exports.sourceNodes = async (
    */
   if (collections && collections.length) {
     for (let i = 0; i < collections.length; i++) {
-      const collectionName = collections[i];
-      const apiUrl = `${normalizedBaseUrl}/${restApiRoutePrefix}/collections/${collectionName}/entries`;
-      const response = await fetch(apiUrl);
-      const { data } = await response.json();
+      try {
+        const collectionName = collections[i];
+        const apiUrl = `${normalizedBaseUrl}/${restApiRoutePrefix}/collections/${collectionName}/entries`;
+        const response = await fetch(apiUrl);
+        const { data } = await response.json();
 
-      data.forEach((item) => {
-        const collectionNameCapitalized = capitalizeName(collectionName);
+        data.forEach((item) => {
+          const collectionNameCapitalized = capitalizeName(collectionName);
 
-        createNode({
-          ...item,
-          id: createNodeId(`${collectionNameCapitalized}-${item.id}`),
-          parent: null,
-          children: [],
-          internal: {
-            type: `Collection${collectionNameCapitalized}`,
-            content: JSON.stringify(item),
-            contentDigest: createContentDigest(item),
-          },
+          createNode({
+            ...item,
+            id: createNodeId(`${collectionNameCapitalized}-${item.id}`),
+            parent: null,
+            children: [],
+            internal: {
+              type: `Collection${collectionNameCapitalized}`,
+              content: JSON.stringify(item),
+              contentDigest: createContentDigest(item),
+            },
+          });
         });
-      });
+      } catch (e) {
+        httpExceptionHandler(e);
+      }
     }
   }
 
@@ -91,26 +94,30 @@ exports.sourceNodes = async (
    */
   if (taxonomies && taxonomies.length) {
     for (let i = 0; i < taxonomies.length; i++) {
-      const taxonomyName = taxonomies[i];
-      const apiUrl = `${normalizedBaseUrl}/${restApiRoutePrefix}/taxonomies/${taxonomyName}/terms`;
-      const response = await fetch(apiUrl);
-      const { data } = await response.json();
+      try {
+        const taxonomyName = taxonomies[i];
+        const apiUrl = `${normalizedBaseUrl}/${restApiRoutePrefix}/taxonomies/${taxonomyName}/terms`;
+        const response = await fetch(apiUrl);
+        const { data } = await response.json();
 
-      data.forEach((item) => {
-        const taxonomyNameCapitalized = capitalizeName(taxonomyName);
+        data.forEach((item) => {
+          const taxonomyNameCapitalized = capitalizeName(taxonomyName);
 
-        createNode({
-          ...item,
-          id: createNodeId(`${taxonomyNameCapitalized}-${item.id}`),
-          parent: null,
-          children: [],
-          internal: {
-            type: `Taxonomy${taxonomyNameCapitalized}`,
-            content: JSON.stringify(item),
-            contentDigest: createContentDigest(item),
-          },
+          createNode({
+            ...item,
+            id: createNodeId(`${taxonomyNameCapitalized}-${item.id}`),
+            parent: null,
+            children: [],
+            internal: {
+              type: `Taxonomy${taxonomyNameCapitalized}`,
+              content: JSON.stringify(item),
+              contentDigest: createContentDigest(item),
+            },
+          });
         });
-      });
+      } catch (error) {
+        httpExceptionHandler(e);
+      }
     }
   }
 
@@ -120,23 +127,27 @@ exports.sourceNodes = async (
    * @returns {Node} - Gatsby Node
    */
   if (globals) {
-    const apiUrl = `${normalizedBaseUrl}/${restApiRoutePrefix}/globals`;
-    const response = await fetch(apiUrl);
-    const { data } = await response.json();
+    try {
+      const apiUrl = `${normalizedBaseUrl}/${restApiRoutePrefix}/globals`;
+      const response = await fetch(apiUrl);
+      const { data } = await response.json();
 
-    data.forEach((item) => {
-      createNode({
-        ...item,
-        id: createNodeId(`Global`),
-        parent: null,
-        children: [],
-        internal: {
-          type: `Globals`,
-          content: JSON.stringify(item),
-          contentDigest: createContentDigest(item),
-        },
+      data.forEach((item) => {
+        createNode({
+          ...item,
+          id: createNodeId(`Global`),
+          parent: null,
+          children: [],
+          internal: {
+            type: `Globals`,
+            content: JSON.stringify(item),
+            contentDigest: createContentDigest(item),
+          },
+        });
       });
-    });
+    } catch (e) {
+      httpExceptionHandler(e);
+    }
   }
 
   /**
@@ -145,23 +156,27 @@ exports.sourceNodes = async (
    * @returns {Node} - Gatsby Node
    */
   if (users) {
-    const apiUrl = `${normalizedBaseUrl}/${restApiRoutePrefix}/users`;
-    const response = await fetch(apiUrl);
-    const { data } = await response.json();
+    try {
+      const apiUrl = `${normalizedBaseUrl}/${restApiRoutePrefix}/users`;
+      const response = await fetch(apiUrl);
+      const { data } = await response.json();
 
-    data.forEach((user) => {
-      createNode({
-        ...user,
-        id: createNodeId(`User-${user.id}`),
-        parent: null,
-        children: [],
-        internal: {
-          type: `Users`,
-          content: JSON.stringify(user),
-          contentDigest: createContentDigest(user),
-        },
+      data.forEach((user) => {
+        createNode({
+          ...user,
+          id: createNodeId(`User-${user.id}`),
+          parent: null,
+          children: [],
+          internal: {
+            type: `Users`,
+            content: JSON.stringify(user),
+            contentDigest: createContentDigest(user),
+          },
+        });
       });
-    });
+    } catch (e) {
+      httpExceptionHandler(e);
+    }
   }
 
   /**
@@ -171,26 +186,30 @@ exports.sourceNodes = async (
    */
   if (assets && assets.length) {
     for (let i = 0; i < assets.length; i++) {
-      const assetName = assets[i];
-      const apiUrl = `${normalizedBaseUrl}/${restApiRoutePrefix}/assets/${assetName}`;
-      const response = await fetch(apiUrl);
-      const { data } = await response.json();
+      try {
+        const assetName = assets[i];
+        const apiUrl = `${normalizedBaseUrl}/${restApiRoutePrefix}/assets/${assetName}`;
+        const response = await fetch(apiUrl);
+        const { data } = await response.json();
 
-      data.forEach((item) => {
-        const assetNameCapitalized = capitalizeName(assetName);
+        data.forEach((item) => {
+          const assetNameCapitalized = capitalizeName(assetName);
 
-        createNode({
-          ...item,
-          id: createNodeId(`${assetNameCapitalized}-${item.id}`),
-          parent: null,
-          children: [],
-          internal: {
-            type: `Assets${assetNameCapitalized}`,
-            content: JSON.stringify(item),
-            contentDigest: createContentDigest(item),
-          },
+          createNode({
+            ...item,
+            id: createNodeId(`${assetNameCapitalized}-${item.id}`),
+            parent: null,
+            children: [],
+            internal: {
+              type: `Assets${assetNameCapitalized}`,
+              content: JSON.stringify(item),
+              contentDigest: createContentDigest(item),
+            },
+          });
         });
-      });
+      } catch (e) {
+        httpExceptionHandler(e);
+      }
     }
   }
 };
